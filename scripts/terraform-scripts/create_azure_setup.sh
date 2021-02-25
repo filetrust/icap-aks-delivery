9e94f66388ac956e2aa04e04ccf720b5915f1864
Matthew Pigram <69460780+mpigram@users.noreply.github.com>
2021-02-25 16:11:32 +0000

    updated small error in backend.tfvars scripts

--- scripts/terraform-scripts/create_azure_setup.sh
+++ scripts/terraform-scripts/create_azure_setup.sh
@@ -32,10 +32,10 @@ echo "container_registry: $CONTAINER_REGISTRY_NAME"
 
 unique=$(uuidgen)
 cat <<EOF >backend.tfvars
-resource_group_name  = $RESOURCE_GROUP_NAME
-storage_account_name = $STORAGE_ACCOUNT_NAME"
-container_name       = $CONTAINER_NAME
-key                  = ${unique}."gw.delivery.terraform.tfstate"
+resource_group_name  = "${RESOURCE_GROUP_NAME}"
+storage_account_name = "${STORAGE_ACCOUNT_NAME}"
+container_name       = "${CONTAINER_NAME}"
+key                  = "${unique}.gw.delivery.terraform.tfstate"
 EOF
 
 cat <<EOF >terraform.tfvars
